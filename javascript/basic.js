fe62848fb1a58f6b5b3b4c92ac95aebebcd4c109
balachandar <balachandhar@gmail.com>
2025-01-09 22:09:55 +0530

    ways to access set elements

--- javascript/basic.js
+++ javascript/basic.js
@@ -21,6 +21,14 @@ obj2.sub=10
 console.log(obj2)
 console.log(obj1);
 console.log(obj2);
-set =new Set(["ii",8])
+set =new Set([1,2,3,4,8])
+/* different ways to access the set elements
+forEach(): Loops through all values, executes a function for each one.
+for...of: Iterates directly over the Set values.
+values(): Returns an iterator for the values, which you can iterate over using a loop.
+entries(): Returns an iterator with [key, value] pairs, useful for key-value operations (though in Set, the key is the same as the value).
+Convert to Array: Spread the Set into an array ([...mySet]) and use array methods.*/ 
 console.log(typeof(set));
 console.log(set);
+console.log(set.values());
+set.forEach(value => console.log(value));
