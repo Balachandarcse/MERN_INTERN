30855bde85f142a0400403b49e78ae3ea2378cf6
balachandar <balachandhar@gmail.com>
2025-01-21 10:07:02 +0530

    event handling executed

--- frontend/src/components/functionalComponents/about.jsx
+++ frontend/src/components/functionalComponents/about.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
+
 const About=()=>{
+    var [text,setText]=useState("yooooo!");
+    function handelText(event){
+        setText(event.target.value)
+    }
     return (
+       
         <>
         <h1>hi</h1>
         <h2>this is about section</h2>
+        {/* <textarea value={text} onChange={()=>setText()}/> */}
+        {/* <textarea value={text} onChange={(event)=>setText(event.target.value)}/> */}
+        <textarea value={text} onChange={handelText}/>
+        <h2>the text is : {text}</h2>
         </>
     )
 }
