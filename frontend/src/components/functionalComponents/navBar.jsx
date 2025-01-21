f4b755f8110131131a5f9870967a55599c761e50
balachandar <balachandhar@gmail.com>
2025-01-21 14:41:50 +0530

    routers executed

--- frontend/src/components/functionalComponents/navBar.jsx
+++ frontend/src/components/functionalComponents/navBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import "../../css/nav.css"
 const NavBar=()=>{
 
@@ -5,10 +6,10 @@ const NavBar=()=>{
             <header>
                 <nav>
                     <ul>
-                        <li>Home</li>
-                        <li>about</li>
-                        <li>gallery</li>
-                        <li>contact</li>
+                        <li><Link to='/' className="link">Home</Link></li>
+                        <li><Link to='/about' className="link">about</Link></li>
+                        <li><Link to='/gallery' className="link">gallery</Link></li>
+                        <li><Link to='/contact' className="link">contact</Link></li>
                     </ul>
                 </nav>
             </header>
