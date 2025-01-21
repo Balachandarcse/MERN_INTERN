329c800094c53cc856910d97b7a738d67a64d2bd
balachandar <balachandhar@gmail.com>
2025-01-21 12:39:57 +0530

    Routers basics executed

---
+++ frontend/src/components/functionalComponents/navBar.jsx
@@ -0,0 +1,18 @@
+import "../../css/nav.css"
+const NavBar=()=>{
+
+    return (
+            <header>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>about</li>
+                        <li>gallery</li>
+                        <li>contact</li>
+                    </ul>
+                </nav>
+            </header>
+    
+    )
+}
+export default NavBar
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import Home from './components/functionalComponents/home.jsx'
 import About from './components/functionalComponents/about.jsx'
 import ClassComponent from './components/classComponents/eg.jsx'
 import Gallery from './components/functionalComponents/gallary.jsx'
 import Contact from './components/functionalComponents/contact.jsx'
+import NavBar from './components/functionalComponents/navBar.jsx'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home/>
-    <About/>
-    <ClassComponent/>
-    <Gallery a="yo!"/>
-    <Contact/>
+    <BrowserRouter>
+    <NavBar/>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/gallery" element={<Gallery/>}></Route>
+    <Route path="/contact" element={<Contact/>}></Route>
+  </Routes>
+    
+    </BrowserRouter>
+    
   </StrictMode>,
 )
